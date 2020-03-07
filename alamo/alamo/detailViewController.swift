0e8c47025545fddd3a89a38c82116f3c59a4ba2f
bala-code <krish090891"gmail.com>
2020-03-07 02:23:20 +0000

    test-final

---
+++ alamo/alamo/detailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  detailViewController.swift
+//  alamo
+//
+//  Created by user165007 on 3/7/20.
+//  Copyright © 2020 user165007. All rights reserved.
+//
+
+import Foundation
+
+/*
+override func viewDidLoad() {
+    super.viewDidLoad()
+    populateInfo()
+}
+
+func populateInfo() {
+    guard let filmsData = film else {
+        return
+    }
+ 
+ */
+
