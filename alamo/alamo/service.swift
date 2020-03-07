85f4f2de88bd788bfd7d06e9007611b247bbd741
bala-code <krish090891"gmail.com>
2020-03-07 02:02:40 +0000

    testing

--- alamo/alamo/service.swift
+++ alamo/alamo/service.swift
@@ -9,35 +9,37 @@
 import Foundation
 import Alamofire
 class Service {
-    fileprivate var baseUrl = ""
-    typealias countriesCallBack = (_ countries:[Country]?, _ Status: Bool, message:String) -> Void
-    var callBack:countriesCallBack?
-    init(baseUrl: String) {
-        self.baseUrl = baseUrl
-    }
-    
-    func getAllCountryNameFrom(endPoint:String)
-    {
-        AF.request(self.baseUrl + endPoint, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { (responseData) in
-          //  print("we got the response")
-            guard let data = responseData.data else {
-                 self.callBack?(nil, false, "")
-                return}
-            do {
-                let countries = try JSONDecoder().decode([Country].self, from: data)
-                self.callBack?(countries, true, "")
-                   // print("countries == \(countries)")
-            } catch {
-                //print("error decoding == \(error)" )
-                self.callBack?(nil, false, error.localizedDescription)
-            }
-            
-        }
-      
-        func completionHandler(callBack: @escaping countriesCallBack) {
-            self.callBack = callBack
+//https://restcountries.eu/rest/v2
+fileprivate var baseUrl = ""
+typealias countriesCallBack = (_ countries:[Country]?, _ status: Bool, _ message:String) -> Void
+
+var callBack:countriesCallBack?
+
+init(baseUrl: String) {
+    self.baseUrl = baseUrl
+}
+
+//MARK:- getAllCountryNameFrom
+func getAllCountryNameFrom(endPoint:String) {
+    AF.request(self.baseUrl + endPoint, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { (responseData) in
+        guard let data = responseData.data else {
+            self.callBack?(nil, false, "")
+
+            return}
+        do {
+        let countries = try JSONDecoder().decode([Country].self, from: data)
+            self.callBack?(countries, true,"")
+        } catch {
+            self.callBack?(nil, false, error.localizedDescription)
         }
         
     }
+}
+
+func completionHandler(callBack: @escaping countriesCallBack) {
+    self.callBack = callBack
+}
+        
+    
     
 }
